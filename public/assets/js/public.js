2bab816e9dd97fd3b80bc498ea99dd38ea54c937
florian <ddjjmm@gmx.de>
2015-06-24 18:54:43 +0200

    https friendly image implementation

--- public/assets/js/public.js
+++ public/assets/js/public.js
@@ -10,9 +10,9 @@
             var card_height = $( this ).data( "hcfw-height" );
             var card_gold = $( this ).data( "hcfw-gold" );
 
-            var replace = '<div class="hcfw-overlay" style="width: '+card_width+'px"><img src="http://wow.zamimg.com/images/hearthstone/cards/'+card_lang+'/medium/'+card_id+'.png" width="'+card_width+'" height="'+card_height+'"/></div>';
+            var replace = '<div class="hcfw-overlay" style="width: '+card_width+'px"><img src="//wow.zamimg.com/images/hearthstone/cards/'+card_lang+'/medium/'+card_id+'.png" width="'+card_width+'" height="'+card_height+'"/></div>';
 
-            var gold = '<div class="hcfw-overlay" style="width: '+card_width+'px"><img src="http://wow.zamimg.com/images/hearthstone/cards/'+card_lang+'/animated/'+card_id+'_premium.gif" width="'+card_width+'" height="'+card_height+'"/></div>';
+            var gold = '<div class="hcfw-overlay" style="width: '+card_width+'px"><img src="//wow.zamimg.com/images/hearthstone/cards/'+card_lang+'/animated/'+card_id+'_premium.gif" width="'+card_width+'" height="'+card_height+'"/></div>';
 
             if ( card_gold ) {
                 replace = gold;
