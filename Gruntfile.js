c0506b504cc0d665c5e920862d383490fa20d099
flowdee <mail@flowdee.de>
2017-03-05 18:33:01 +0100

    updated local data files to v17720 resolves #56

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,70 +6,25 @@ module.exports = function (grunt) {
 
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
-        downloadfile: {
-            files: [
-                // Cards
-                {
-                    url: 'https://api.hearthstonejson.com/v1/15590/deDE/cards.json',
-                    port: 80,
-                    dest: 'dist/deDE/',
-                    name: 'cards.json',
-                    overwrite: true
-                },
-                {
-                    url: 'https://api.hearthstonejson.com/v1/15590/enUS/cards.json',
-                    port: 80,
-                    dest: 'dist/enUS/',
-                    name: 'cards.json',
-                    overwrite: true
-                },
-                {
-                    url: 'https://api.hearthstonejson.com/v1/15590/esES/cards.json',
-                    port: 80,
-                    dest: 'dist/esES/',
-                    name: 'cards.json',
-                    overwrite: true
-                },
-                {
-                    url: 'https://api.hearthstonejson.com/v1/15590/frFR/cards.json',
-                    port: 80,
-                    dest: 'dist/frFR/',
-                    name: 'cards.json',
-                    overwrite: true
-                },
-                {
-                    url: 'https://api.hearthstonejson.com/v1/15590/ptBR/cards.json',
-                    port: 80,
-                    dest: 'dist/ptBR/',
-                    name: 'cards.json',
-                    overwrite: true
-                },
-                {
-                    url: 'https://api.hearthstonejson.com/v1/15590/ruRU/cards.json',
-                    port: 80,
-                    dest: 'dist/ruRU/',
-                    name: 'cards.json',
-                    overwrite: true
-                },
-                {
-                    url: 'https://api.hearthstonejson.com/v1/15590/zhCN/cards.json',
-                    port: 80,
-                    dest: 'dist/zhCN/',
-                    name: 'cards.json',
-                    overwrite: true
-                },
-                // Images
-                {
-                    url: 'https://github.com/schmich/hearthstone-card-images/blob/master/images.json',
-                    port: 80,
-                    dest: 'dist/',
-                    name: 'images.json',
-                    overwrite: true
-                }
+        clean: {
+            deploy: [
+                'dist/*'
             ]
         },
+        curl: {
+            // Images
+            'dist/images.json': 'https://github.com/schmich/hearthstone-card-images/blob/master/images.json',
+            // Cards
+            'dist/deDE/cards.json': 'https://api.hearthstonejson.com/v1/17720/deDE/cards.json',
+            'dist/enUS/cards.json': 'https://api.hearthstonejson.com/v1/17720/enUS/cards.json',
+            'dist/esES/cards.json': 'https://api.hearthstonejson.com/v1/17720/esES/cards.json',
+            'dist/frFR/cards.json': 'https://api.hearthstonejson.com/v1/17720/frFR/cards.json',
+            'dist/ptBR/cards.json': 'https://api.hearthstonejson.com/v1/17720/ptBR/cards.json',
+            'dist/ruRU/cards.json': 'https://api.hearthstonejson.com/v1/17720/ruRU/cards.json',
+            'dist/zhCN/cards.json': 'https://api.hearthstonejson.com/v1/17720/zhCN/cards.json'
+        }
     });
 
     // Download coards
-    grunt.registerTask( 'download', [ 'downloadfile' ] );
+    grunt.registerTask( 'download', [ 'clean', 'curl' ] );
 };
