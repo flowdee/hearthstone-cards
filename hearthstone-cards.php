f714f42ccc5eab60becf39f2f093b8eb59f67cb3
flowdee <ddjjmm@gmx.de>
2017-12-09 10:25:18 +0100

    fixed author url

--- hearthstone-cards.php
+++ hearthstone-cards.php
@@ -5,7 +5,7 @@
  * Description:       Hearthstone Cards for WordPress adds an overlay to written card names and displays the associated image while hovering them.
  * Version:           2.4.2
  * Author:            KryptoniteWP
- * Author URI:        https://kryptoniteWP
+ * Author URI:        https://kryptonitewp.com
  * Text Domain:       hearthstone-cards
  * License:           GPL-3.0+
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
